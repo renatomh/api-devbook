ec126a7a55f82f99f14897206fdd8ec14bce1c1a
Renato Marino Henz <renatomarinohenz@gmail.com>
2023-01-07 18:25:00 -0300

    fix: user model

--- src/models/User.go
+++ src/models/User.go
@@ -4,10 +4,10 @@ import "time"
 
 // User represents a social network user
 type User struct {
-	ID        uint64    `json:"id.onitempty"`
-	Name      string    `json:"name.onitempty"`
-	Username  string    `json:"username.onitempty"`
-	Email     string    `json:"email.onitempty"`
-	Pass      string    `json:"pass.onitempty"`
-	CreatedAt time.Time `json:"createdAt.onitempty"`
+	ID        uint64    `json:"id,onitempty"`
+	Name      string    `json:"name,onitempty"`
+	Username  string    `json:"username,onitempty"`
+	Email     string    `json:"email,onitempty"`
+	Pass      string    `json:"pass,onitempty"`
+	CreatedAt time.Time `json:"createdAt,onitempty"`
 }
